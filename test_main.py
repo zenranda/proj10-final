7f002ea1741ba7a3f89cc791e652ba3605b331bf
zenranda <XanderJones@hushmail.com>
2016-12-11 03:51:34 -0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,8 @@
 ###
 #Various nose tests. If you want to adapt this for your own use, be aware that the start/end block list has a very specific formatting.
 ###
-import get_freebusy
+import date_chopper
 import arrow
-from operator import itemgetter
 
 from pymongo import MongoClient
 import secrets.admin_secrets
@@ -24,36 +23,35 @@ except:
     print("Failure opening database.  Is Mongo running? Correct password?")
     sys.exit(1)  
 
-
-
-def test_free_times():      #Given a sample list, check to see if it's getting free/busy blocks correctly
-    print("RESULTS:")
-    print(get_freebusy.get_freebusy([['2016-11-20T08:30:00-08:00', '2016-11-20T010:30:00-08:00'], ['2016-11-20T11:00:00-08:00', '2016-11-20T15:00:00-08:00'], ['2016-11-20T16:30:00-08:00', '2016-11-20T19:00:00-08:00'], ['2016-11-24T13:30:00-08:00', '2016-11-24T16:00:00-08:00'], ['2016-11-21T15:00:00-08:00', '2016-11-21T18:30:00-08:00']], arrow.get('2016-11-23T16:00:00'), arrow.get('2016-11-24T04:00:00')))
-    print(get_freebusy.get_freebusy([], arrow.get('2016-11-20T20:00:00'),arrow.get('2016-11-24T04:00:00')))
-
-    ranges = [['2016-11-20T08:30:00-08:00', '2016-11-20T010:30:00-08:00'], ['2016-11-20T11:00:00-08:00', '2016-11-20T15:00:00-08:00'], ['2016-11-20T16:30:00-08:00', '2016-11-20T19:00:00-08:00'], ['2016-11-24T13:30:00-08:00', '2016-11-24T16:00:00-08:00'], ['2016-11-21T15:00:00-08:00', '2016-11-21T18:30:00-08:00']]
-    start = arrow.get('2016-11-23T16:00:00')
-    end = arrow.get('2016-11-24T04:00:00')
-
-    assert get_freebusy.get_freebusy(ranges, start, end) == [['At 2016-11-20 from 08:00:00 to 08:30:00', 'At 2016-11-20 from 10:30:00 to 11:00:00', 'At 2016-11-20 from 15:00:00 to 16:30:00', 'At 2016-11-20 from 19:00:00 to 20:00:00', 'At 2016-11-21 from 08:00:00 to 15:00:00', 'At 2016-11-21 from 18:00:00 to 20:00:00', 'At 2016-11-24 from 08:00:00 to 13:30:00', 'At 2016-11-24 from 16:00:00 to 20:00:00'], ['At 2016-11-20 from 08:30:00 to 10:30:00', 'At 2016-11-20 from 11:00:00 to 15:00:00', 'At 2016-11-20 from 16:30:00 to 19:00:00', 'At 2016-11-21 from 15:00:00 to 18:00:00', 'At 2016-11-24 from 13:30:00 to 16:00:00']]
+def test_overlap():      #Given a sample list, check to see if its dates overlap at all
+    ranges = [['2016-11-20T08:30:00', '2016-11-20T10:30:00'], ['2016-11-20T11:00:00', '2016-11-20T15:00:00']]
+    assert (date_chopper.date_overlap(ranges)) == False
+    
+    ranges = [['2016-11-20T08:30:00', '2016-11-20T10:30:00'], ['2016-11-20T11:00:00', '2016-11-20T15:00:00'], ['2016-11-20T16:00:00', '2016-11-20T17:00:00'], ['2016-11-20T17:00:00', '2016-11-20T19:00:00']]
+    assert (date_chopper.date_overlap(ranges)) == True
+    
+    ranges = [['2016-11-20T08:30:00', '2016-11-20T10:30:00'], ['2016-11-20T10:30:00', '2016-11-20T13:00:00'], ['2016-11-20T13:00:00', '2016-11-20T14:00:00'], ['2016-11-20T14:10:00', '2016-11-20T16:00:00']]
+    assert (date_chopper.date_overlap(ranges)) == True
   
-  
-    ranges = []
-    start = arrow.get('2016-11-20T20:00:00')
-    end = arrow.get('2016-11-24T04:00:00')
+def test_underlap():         #tests if the program can detect start times that go out of bounds
+    ranges = [['2016-11-20T08:30:00', '2016-11-20T10:30:00'], ['2016-11-20T11:00:00', '2016-11-20T15:00:00']]
+    start = arrow.get('2016-11-20T05:00:00')
+    assert (date_chopper.date_underlap(ranges, start)) == True
     
-    assert get_freebusy.get_freebusy(ranges, start, end) == [[], []]
+    ranges = [['2016-11-20T08:30:00', '2016-11-20T10:30:00'], ['2016-11-20T11:00:00', '2016-11-20T15:00:00']]
+    start = arrow.get('2016-11-20T10:00:00')
+    assert (date_chopper.date_underlap(ranges, start)) == False
     
-    
-def test_overlap():         #tests if the program can handle dates that overlap/intersect
-    ranges = [['2016-11-22T11:00:00-08:00', '2016-11-22T16:00:00-08:00'], ['2016-11-23T12:00:00-08:00', '2016-11-23T15:30:00-08:00']]
+    ranges = [['2016-11-20T08:30:00', '2016-11-20T10:30:00'], ['2016-11-20T11:00:00', '2016-11-20T15:00:00']]
     start = arrow.get('2016-11-20T16:00:00')
-    end = arrow.get('2016-11-24T04:00:00')
+    assert (date_chopper.date_underlap(ranges, start)) == False
     
-    assert get_freebusy.get_freebusy(ranges, start, end) == [['At 2016-11-22 from 08:00:00 to 11:00:00', 'At 2016-11-22 from 16:00:00 to 20:00:00', 'At 2016-11-23 from 08:00:00 to 11:00:00', 'At 2016-11-23 from 18:30:00 to 20:00:00'], ['At 2016-11-22 from 11:00:00 to 16:00:00', 'At 2016-11-23 from 11:00:00 to 18:30:00']]
-
+    ranges = [['2016-11-20T08:30:00', '2016-11-20T10:30:00'], ['2016-11-20T11:00:00', '2016-11-20T15:00:00']]
+    start = arrow.get('2016-11-20T10:00:00')
+    assert (date_chopper.date_underlap(ranges, start)) == True
     
-def test_db():
+    
+def test_db():                      #tests basic DB operation
     assert collection != None
     collection.insert({"type" : "freebusy", "entry" : [["entry 1"], ["entry 2"]]})
     assert base_size < collection.count()
